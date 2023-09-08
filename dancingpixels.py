3ff92064fe04ff64aaf9301ef5e5bb98c7e6141c
HyperBitGore <42724327+HyperBitGore@users.noreply.github.com>
2023-09-07 23:34:49 -0500

    dancing pixels
    makes neo pixels dance

---
+++ dancingpixels.py
@@ -0,0 +1,48 @@
+# Nathan Moore
+# starting from from adafruit example
+# https://learn.adafruit.com/welcome-to-circuitpython/creating-and-editing-code
+#
+import random
+import board
+import digitalio
+import time
+from adafruit_circuitplayground import cp
+
+# set up the (red) LED
+#led = digitalio.DigitalInOut(board.D13)
+#led.direction = digitalio.Direction.OUTPUT
+cp.pixels.brightness = 0.02
+cp.pixels.fill((50, 50, 50))
+
+colors = {(255, 50, 50), (25, 255, 25), (25, 25, 255), (150, 150, 50), (150, 50, 150), (50, 150, 150), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)}
+
+up = False
+speed = 0.1
+
+while True:
+    if(up):
+        speed += 0.01
+    else:
+        speed -= 0.01
+
+    if(speed <= 0.04):
+        speed = 0.04
+        up = True
+    elif(speed >= 0.1):
+        speed = 0.1
+        up = False
+
+    speed = 0.03
+    for pn in range(0,10,1):
+        col1 = (random.randint(25, 255), random.randint(25, 255), random.randint(25, 255))
+        cp.pixels[pn] = col1
+        time.sleep(speed)
+        speed += 0.01
+        cp.pixels[pn] = (0, 0, 0)
+    speed = 0.03
+    for pn in range(9,-1,-1):
+        col1 = (random.randint(25, 255), random.randint(25, 255), random.randint(25, 255))
+        cp.pixels[pn] = col1
+        time.sleep(speed)
+        speed += 0.01
+        cp.pixels[pn] = (0, 0, 0)
